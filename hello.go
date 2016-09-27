ad153e1ee0ff29b00a45495930607ab9fcf519ae
xerix <sgphotowalker@gmail.com>
2016-09-27 21:36:54 +0800

    inital commit

---
+++ hello.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+    "fmt"
+    "github.com/xerix/stringutil"
+)
+
+func main() {
+    fmt.Printf("Hello World!\n")
+    fmt.Printf(stringutil.Reverse("\nHello World!"))
+}
